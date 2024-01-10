79c8a81016b3cc97be2910a659b9e441a53db8c8
Harshul Jain <harshulgangwal02@gmail.com>
2024-01-10 20:20:54 +0530

    Added Navigation

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function HomeScreen(){
+  return (
+    <View>
+    <Text className='text-black items-center justify-center'>Hello People</Text>
+    </View>
+  )
+}
